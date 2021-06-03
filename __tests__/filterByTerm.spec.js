ac763df9e022bfda922b798b7485024b91293d1c
Ethan Gough <dante1498@gmail.com>
2021-06-03 16:28:17 +0100

    Fix search terms to include any capitalised searches for tests

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,19 +1,23 @@
-function filterByTerm(inputArr, searchTerm) {
-    return inputArr.filter(function(arrayElement) {
-      return arrayElement.url.match(searchTerm);
-    });
-  }
-  
-  describe("Filter function", () => {
+describe("Filter function", () => {
     test("it should filter by a search term (link)", () => {
-      const input = [
-        { id: 1, url: "https://github.com/Ethan1498" },
-        { id: 2, url: "https://github.com/Ethan1498?tab=projects" },
-        { id: 3, url: "https://github.com/Ethan1498?tab=repositories" }
-      ];
+        const input = [
+            { id: 1, url: "https://github.com/Ethan1498" },
+            { id: 2, url: "https://github.com/Ethan1498?tab=projects" },
+            { id: 3, url: "https://github.com/Ethan1498?tab=repositories" }
+        ];
   
       const output = [{ id: 3, url: "https://github.com/Ethan1498?tab=repositories" }];
   
-      expect(filterByTerm(input, "repositories")).toEqual(output);
+      expect(filterByTerm(input, "repositories")).toEqual(output); // Searches the const for the word repositories, will pass test
+      
+      expect(filterByTerm(input, "REPOSITORIES")).toEqual(output); // this will fail test, as its searching for uppercaes
+
     });
-  });
+});
+
+function filterByTerm(inputArr, searchTerm) {
+    const regex = new RegExp(searchTerm, "i");
+    return inputArr.filter(function(arrayElement) {
+        return arrayElement.url.match(regex);
+    });
+}
