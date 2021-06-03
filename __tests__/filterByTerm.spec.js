16eb3469c69e27866560bc44f92d72a1a778728a
Ethan Gough <dante1498@gmail.com>
2021-06-03 16:40:33 +0100

    Trying to get another test case working

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,3 +1,4 @@
+const filterByTerm = require("../src/filterByTerm");
 describe("Filter function", () => {
     test("it should filter by a search term (link)", () => {
         const input = [
@@ -13,11 +14,18 @@ describe("Filter function", () => {
       expect(filterByTerm(input, "REPOSITORIES")).toEqual(output); // this will fail test, as its searching for uppercaes
 
     });
-});
+    test("it should filter by a search term (github)", () => {
+        const input = [
+            { id: 1, url: "https://github.com/Ethan1498" },
+            { id: 2, url: "https://github.com/Ethan1498?tab=projects" },
+            { id: 3, url: "https://github.com/Ethan1498?tab=repositories" }
+        ];
 
-function filterByTerm(inputArr, searchTerm) {
-    const regex = new RegExp(searchTerm, "i");
-    return inputArr.filter(function(arrayElement) {
-        return arrayElement.url.match(regex);
+        const output = [
+            { id: 1, url: "https://github.com/Ethan1498" },
+            { id: 2, url: "https://github.com/Ethan1498?tab=projects" },
+        ];
+
+        expect(filterByTerm(input, "github")).toEqual(output);
     });
-}
+});
