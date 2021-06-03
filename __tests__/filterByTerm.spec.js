92b5b1857366f512b1a380f4f47702b50f88d1b4
Ethan Gough <dante1498@gmail.com>
2021-06-03 16:08:56 +0100

    Further testing, test wont pass yet

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,14 +1,18 @@
+function filterByTerm(inputArr, searchTerm) {
+    return inputArr.filter(function(arrayElement) {
+        return arrayElement.name.match(searchTerm);
+    });
+}
+
 describe("Filter Function", () => {
-    // tests here
     test("it should filter by a search term (name)", () => {
-        // actual tests
         const input = [
             {id: 1, name: "Bob"},
             {id: 2, name: "John"},
             {id: 3, name: "Elliot"}
         ];
 
-        const output = [{ id: 3, name: "Elliot"}];
+        const output = [{ id: 3, name: "Elliot" }];
 
         expect(filterByTerm(input, "name")).toEqual(output);
     });
